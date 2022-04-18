d85beff9feec180b9d69a2b7a766c4a744e7196a
Nolan <trongson2k@gmail.com>
2022-04-19 02:22:15 +0700

    v1.0.2
    login module

--- MVC_TTCS/js/main.js
+++ MVC_TTCS/js/main.js
@@ -1,4 +1,4 @@
-    let openLogin = document.getElementsByClassName('open_login')
+﻿    let openLogin = document.getElementsByClassName('open_login')
     let openSignup = document.getElementsByClassName('open_signup')
     //hide
     let modal_login = document.getElementById("loginnnn")
@@ -70,9 +70,50 @@
     var TK_sign = document.getElementById("username_sign");
     var MK_sign = document.getElementById("pass_sign");
     var otp = document.getElementById("adminnnn");
-    var notifi=document.getElementById("noti");
-   
-    
+    var notifi = document.getElementById("noti");
 
+
+    var btn_sigin = document.getElementById("btn_sigin");
+    var btn_login = document.getElementById("btn_login");
+    var user_name = document.getElementById('user_name');
+    var login_done = document.getElementById('login_done');
+   
+    var database_pick = ''
+
+
+    function database_selec_click(database_pick_select) {
+        database_pick = database_pick_select.value;
+        console.log(database_pick)
+    }
+    function xuly_login() {
+        if (TK_login.value == '' && MK_login.value == '') {
+            login_done.style.height = "450px";
+            notifi.innerHTML = "không đước để trống ô tài khoản, mật khẩu";
+        }
+        else if (TK_login.value == "") {
+            login_done.style.height = "450px";
+            notifi.innerHTML = "không đước để trống ô tài khoản";
+        }
+        else if (MK_login.value == "") {
+            login_done.style.height = "450px";
+            notifi.innerHTML = "không đước để trống ô mật khẩu";
+        }
+        else if (database_pick === 'nooo' || database_pick === '' || database_pick === 'defaultt' || database_pick == false) {
+            login_done.style.height = "450px";
+            notifi.innerHTML = "hãy chọn data";
+        }
+        else {
+            login_done.style.height = "450px";
+            notifi.innerHTML = "thành công";
+            modal_signup.style.display = "none";
+            modal_login.style.display = "none";
+            btn_login.style.display = 'none';
+            btn_sigin.style.display = 'none';
+            user_name.innerHTML="1101010"
+            console.log(databased_pick);
+            console.log('done')
+
+        }
+    }
 
 
